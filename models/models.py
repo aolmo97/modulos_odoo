98a846a823cfd053200a9daa4c81523f89523b99
Antonio <aolmolopez97@gmail.com>
2019-05-13 22:26:03 +0200

    Creacion de modelo y vista de terricolas

--- models/models.py
+++ models/models.py
@@ -8,4 +8,17 @@ class Terricolas(models.Model):
  descripcion=fields.Char(string="Descripcion")
  edad=fields.Integer(string="Edad",required=True)
  fecha_nac=fields.Date(string="Fecha Nacimiento",required=True)
-    #casado
+ #casado relacion uno a uno
+ #casado=fields.
+ #hijos relaciono uno a muchos
+ #hijos=fields.One2many('dragonball_z.terricolas', 'nombre')
+ hijos=fields.Many2many('dragonball_z.terricolas', 'nombre')
+class Namekiano(models.Model):
+ _name = 'dragonball_z.namekiano'
+ _rec_name= 'nombre_namekiano'
+ nombre_namekiano = fields.Char(string="Nombre",required=True)
+ descripcion=fields.Char(string="Descripcion")
+ edad=fields.Integer(string="Edad",required=True)
+ fecha_nac=fields.Date(string="Fecha Nacimiento",required=True)
+ #casado relacion uno a uno
+ #hijos relaciono uno a muchos 
