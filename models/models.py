95a4e93e0432a890aea3139fc017be8e05b6f2aa
Antonio <aolmolopez97@gmail.com>
2019-05-13 21:30:53 +0200

    Creacion de modelo y vista de terricolas

--- models/models.py
+++ models/models.py
@@ -2,11 +2,10 @@
 
 from odoo import models, fields, api
 class Terricolas(models.Model):
-    _name = 'dragon_ball_z.terricolas'
-
-    _rec_name= 'nombre'
-    nombre = fields.Char(string="Nombre",required=True)
-    descripcion=fields.Char(string="Descripcion")
-    edad=fields.Integer(string="Edad",required=True)
-    fecha_nac=fields.Date(string="Fecha Nacimiento",required=True)
+ _name = 'dragon_ball_z.terricolas'
+ _rec_name= 'nombre'
+ nombre = fields.Char(string="Nombre",required=True)
+ descripcion=fields.Char(string="Descripcion")
+ edad=fields.Integer(string="Edad",required=True)
+ fecha_nac=fields.Date(string="Fecha Nacimiento",required=True)
     #casado
