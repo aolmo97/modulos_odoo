2c24c0b1d1bab33f0977a594f219c1e08a828d38
Antonio <aolmolopez97@gmail.com>
2019-05-13 21:08:30 +0200

    Creacion de modelo y vista de terricolas

--- __manifest__.py
+++ __manifest__.py
@@ -3,20 +3,19 @@
     'name': "dragon_ball_z",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Modulo de Dragon Ball Z""",
 
     'description': """
-        Long description of module's purpose
+        Modulo de Dragon Ball Z
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Antonio Olmo Lopez",
+    'website': "https://www.a2mkt.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/10.0/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Ciencia ficcion',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -25,7 +24,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
+        'views/terricolas.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
--- models/models.py
+++ models/models.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+class terricolas(models.Model):
+    _name = 'dragon_ball_z.terricolas'
+    _rec_name= 'nombre'
 
-# class dragon_ball_z(models.Model):
-#     _name = 'dragon_ball_z.dragon_ball_z'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    nombre = fields.Char(string="Nombre",required=True)
+    descripcion=fields.Char(string="Descripcion")
+    edad=fields.Integer(string="Edad",required=True)
+    fecha_nac=fields.Date(string="Fecha Nacimiento",required=True)
+    #casado
