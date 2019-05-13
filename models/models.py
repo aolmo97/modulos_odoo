2472b0367e5be1a71344dcd38f47ce7ef84be897
Antonio <aolmolopez97@gmail.com>
2019-05-13 22:52:38 +0200

    Creacion de modelo y vista de terricolas

--- models/models.py
+++ models/models.py
@@ -17,9 +17,9 @@ class Namekiano(models.Model):
  _name = 'dragonball_z.namekiano'
  _rec_name= 'nombre_namekiano'
  nombre_namekiano = fields.Char(string="Nombre",required=True)
- descripcion=fields.Char(string="Descripcion")
- edad=fields.Integer(string="Edad",required=True)
- fecha_nac=fields.Date(string="Fecha Nacimiento",required=True)
+ descripcion_namekiano=fields.Char(string="Descripcion")
+ edad_namekiano=fields.Integer(string="Edad",required=True)
+ fecha_nac_namekiano=fields.Date(string="Fecha Nacimiento",required=True)
 #  amigos_terricolas=fields.One2Many('dragonball_z.terricolas',
 #  'nombre')
  #casado relacion uno a uno
